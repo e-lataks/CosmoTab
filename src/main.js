5abbe51f26f3e9d98a9c05c7418416d01c839026
e-lataks <emiklataks600@gmail.com>
2026-08-08 17:09:37 +0300

    Add CSS and basic styling

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
+import './style.css';
+
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
-document.querySelector("#app").innerHTML = "<p>Loading...</p>";
+document.querySelector("#app").innerHTML = "Loading...";
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-        let media;
+  .then(response => response.json())
+  .then(data => {
+    let media;
 
-        if (data.media_type === "image") {
-            media = `<img src="${data.url}"/>`;
-        } else {
-            media = `<video src="${data.url}" controls></video>`;
-        }
+    if (data.media_type === "image") {
+      media = `<img src="${data.url}">`;
+    } else {
+      media = `<video src="${data.url}" controls></video>`;
+    }
 
-        document.querySelector("#app").innerHTML = `
-            <h1>${data.title}</h1>
-            ${media}
-            <p>${data.explanation}</p>
-        `;
-})
-.catch(err => {
-    document.querySelector("#app").innerHTML = 
-        `<P>Error: ${err.message}</p>`;
-});
+    document.querySelector("#app").innerHTML = `
+      <h1>${data.title}</h1>
+      ${media}
+      <p>${data.explanation}</p>
+    `;
+  })
+  .catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+  });
